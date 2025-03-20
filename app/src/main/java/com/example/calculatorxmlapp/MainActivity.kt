ce9ba159b031ebea3724c174d9a686eb35afb608
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-20 23:19:37 +0100

    Add selecting mathematical operations.

--- app/src/main/java/com/example/calculatorxmlapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorxmlapp/MainActivity.kt
@@ -8,12 +8,16 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity(){
     private var numberButtons= mutableListOf<Button>()
+    private var operatorButtons= mutableListOf<Button>()
     private var numbers= mutableListOf<Int>()
     private var tvInput:TextView?=null;
     private var clrBtn:Button?=null;
     private var lastNumeric:Boolean=false;
     private var hasDot:Boolean=false;
     private var dotButton:Button?=null;
+
+    private val operations = arrayOf("Div","Mult","Add","Min");
+    private val operators = arrayOf("/","+","-","*");
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +27,12 @@ class MainActivity : AppCompatActivity(){
         dotButton = findViewById(R.id.buttonDot);
         clrBtn?.setOnClickListener { onClear() }
         dotButton?.setOnClickListener { onDecimalPoint() }
+        for (i in operations)
+        {
+            val btn: Button = (findViewById(resources.getIdentifier("button$i", "id", packageName)));
+            numberButtons.add(btn);
+            btn.setOnClickListener { onOperator(btn) }
+        }
         for(i in 0..9) {
 
             val btn: Button = (findViewById(resources.getIdentifier("button$i", "id", packageName)));
@@ -53,4 +63,28 @@ class MainActivity : AppCompatActivity(){
           //Toast.makeText(this, btn.text, Toast.LENGTH_LONG).show();
           tvInput?.append(btn.text);
       }
+    private fun onOperator(view:View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text);
+                lastNumeric=false;
+                hasDot=false;
+            }
+        }
+
+    }
+
+    private fun isOperatorAdded(value:String):Boolean
+    {
+        return if(value.startsWith("-"))
+        {
+            false
+        }
+        else
+        {
+            val match = operators.filter{it in value  }
+            return match.isNotEmpty()
+        }
+    }
+
 }
